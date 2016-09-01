c70083ace32cc4153432c5ea6d3adf8f4b0ce8fa
Rick Thornbrugh <Rick Thornbrugh>
2016-09-01 14:43:29 -0700

    delete rental, and city functions working.

--- app/routes/city.js
+++ app/routes/city.js
@@ -5,6 +5,7 @@ export default Ember.Route.extend({
     return this.store.findRecord('city', params.city_id);
   },
   actions: {
+    save3(params){
     var newRental = this.store.createRecord('rental', params);
     var city = params.city;
     city.get('rentals').addObject(newRental);
@@ -12,6 +13,15 @@ export default Ember.Route.extend({
       return city.save();
     });
     this.transitionTo('city', params.city)
+  },
+  destroyCity(city) {
+    var rental_deletions = city.get('rentals').map(function(rental){
+      return rental.destroyRecord();
+    });
+    Ember.RSVP.all(rental_deletions).then(function(){
+      return city.destroyRecord();
+    });
+    this.transitionTo('index');
   }
-
+}
 });
